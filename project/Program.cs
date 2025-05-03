ccb2eb6dd72137ceac3da29f96c85a7acef4a904
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-05-03 17:22:07 +0200

    Calculate sums

--- project/Program.cs
+++ project/Program.cs
@@ -23,3 +23,8 @@ int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
+
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
